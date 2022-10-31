eaca582afc2bce48d320eb3a79aebd617979c078
Kseniya Yatskevich <kseniya_yatskevich@epam.com>
2022-10-31 19:19:17 +0400

    fix: removed unusable code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
-import Image from './asset/images/react.png';
-import Icon from './asset/images/highlight-star.svg';
-
 import './styles.scss';
 
 export const App = () => {
-    return (
-        <>
-            <h1>
-                Hello React {process.env.NODE_ENV} {process.env.name}
-            </h1>
-            <img src={Image} alt="logo" />
-            <img src={Icon} alt="logo" width="30" />
-        </>
-    );
+    return <h1>Hello React</h1>;
 };
